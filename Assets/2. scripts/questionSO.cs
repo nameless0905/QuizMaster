7cef7636dda3f50bbbd3a0a441033eae1bbb5afe
nameless0905 <vahyeon2515@sonline20.sen.go.kr>
2025-08-14 11:41:10 +0900

    웅나나

--- Assets/2. scripts/questionSO.cs
+++ Assets/2. scripts/questionSO.cs
@@ -3,7 +3,24 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class questionSO : ScriptableObject
 {
-    [TextArea(2,6)]
-    [SerializeField]string question = "여기에 질문을 입력하세요.";
-
+    [TextArea(2,6)  ]
+    [SerializeField] string question = "여기에 질문을 입력하세요.";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex = 0;
+    public string GetQuestion()
+    {
+        return question;
+    }
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+    public string GetCorrectAnswer()
+    {
+        return answers[correctAnswerIndex];
+    }
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
