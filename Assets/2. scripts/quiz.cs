5a9924c01b14ebe2a738439ad615a371e6aa158d
nameless0905 <vahyeon2515@sonline20.sen.go.kr>
2025-08-22 11:29:00 +0900

    asd

--- Assets/2. scripts/quiz.cs
+++ Assets/2. scripts/quiz.cs
@@ -1,19 +1,32 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class quiz : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] questionSO question;
-    [SerializeField] TextMeshProUGUI[] answerTexts;
+    //[SerializeField] TextMeshProUGUI[] answerTexts;
+    [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] GameObject[] answerButtons;
     void Start()
     {
         questionText.text = question.GetQuestion();
 
-        for (int i = 0; i < answerTexts.Length; i++)
+        for (int i = 0; i < answerButtons.Length; i++)
         {
-            answerTexts[i].text = question.GetAnswer(i);
+            answerButtons[i].GetComponentInChildren<TextMeshProUGUI>().text = question.GetAnswer(i);
+            //answerTexts[i].text = question.GetAnswer(i);
         }
 
+    }
+    public void OnAnswerButtonClicked(int index)
+    {
+        if (index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            answerButtons[index].GetComponent<Image>().sprite = correctAnswerSprite;
+        }
     }
 }
